cd250d750debd69bf116bca63d169ee014e911f7
Yaroslav <yaroslav.rukavets@gmail.com>
2020-02-16 23:06:43 +0200

    Доработал некоторые моменты

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,11 @@ var FONT_GAP = 15;
 var BAR_WIDTH = 40;
 var barHeight = CLOUD_HEIGHT - 120;
 
+function getRandomInt(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -39,12 +44,15 @@ window.renderStatistics = function (ctx, players, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
 
     ctx.fillText(players[i], CLOUD_X + GAP + FONT_GAP + (GAP * 2 + BAR_WIDTH) * i, 270);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + FONT_GAP + (GAP * 2 + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_GAP * 2 - ((barHeight * times[i]) / maxTime));
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(255, '+getRandomInt(0, 100)+'%, 50%)';
     }
     ctx.fillRect(CLOUD_X + GAP * 2 + (GAP * 2 + BAR_WIDTH) * i, 250, BAR_WIDTH, (1 - (barHeight * times[i]) / maxTime));
-
   }
 };
