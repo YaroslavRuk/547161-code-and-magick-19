9a9dd626bcfa5114c238714eea14e0bc87c6fb86
Yaroslav <yaroslav.rukavets@gmail.com>
2020-02-18 12:50:45 +0200

    Оптимизировал код

--- js/stat.js
+++ js/stat.js
@@ -13,7 +13,6 @@ function getRandomInt(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -37,22 +36,21 @@ window.renderStatistics = function (ctx, players, times) {
 
   ctx.fillStyle = '#000';
 
+  var congratulationTextX = CLOUD_X + GAP * 3;
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 20);
-  ctx.fillText('Список результатов:', 120, 30);
+  ctx.fillText('Ура вы победили!', congratulationTextX, CLOUD_Y * 4);
+  ctx.fillText('Список результатов:', congratulationTextX, CLOUD_Y * 6);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
+    var randomBarHeight = (barHeight * times[i]) / maxTime;
+    var gapAndBarWidth = (GAP * 5 + BAR_WIDTH) * i;
+    var gapBetweenColumns = CLOUD_X + GAP + FONT_GAP + gapAndBarWidth;
     ctx.fillStyle = '#000';
-
-    ctx.fillText(players[i], CLOUD_X + GAP + FONT_GAP + (GAP * 2 + BAR_WIDTH) * i, 270);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + FONT_GAP + (GAP * 2 + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_GAP * 2 - ((barHeight * times[i]) / maxTime));
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(255, ' + getRandomInt(0, 100) + '%, 50%)';
-    }
-    ctx.fillRect(CLOUD_X + GAP * 2 + (GAP * 2 + BAR_WIDTH) * i, 250, BAR_WIDTH, (1 - (barHeight * times[i]) / maxTime));
+    ctx.fillText(players[i], gapBetweenColumns, CLOUD_HEIGHT);
+    ctx.fillText(Math.round(times[i]), gapBetweenColumns, CLOUD_HEIGHT - FONT_GAP * 2 - randomBarHeight);
+    ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsl(255, ' + getRandomInt(0, 100) + '%, 50%)';
+    ctx.fillRect(CLOUD_X + GAP * 2 + gapAndBarWidth, CLOUD_HEIGHT - GAP * 2, BAR_WIDTH, (1 - randomBarHeight));
   }
 };
