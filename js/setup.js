bd1ae7a151c4a0638593feba03c0c2dad7f73c8c
Yaroslav <yaroslav.rukavets@gmail.com>
2020-03-01 17:04:03 +0200

    Правки

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNT = 4;
 var userDialog = document.querySelector('.setup');
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -14,22 +15,17 @@ function getRandomElement(randomArray) {
   return randomArray[Math.floor(Math.random() * randomArray.length)];
 }
 
-function init() {
-  return userDialog.classList.remove('hidden');
-}
-init();
-
-var wizards = [];
 function createWizardsProperties() {
-  for (var i = 0; i < 4; i++) {
-    wizards[i] = {};
-    wizards[i].name = getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SECOND_NAMES);
-    wizards[i].coatColor = getRandomElement(COATS);
-    wizards[i].eyesColor = getRandomElement(EYES);
+  var wizards = [];
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    wizards.push({
+      name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SECOND_NAMES),
+      coatColor: getRandomElement(COATS),
+      eyesColor: getRandomElement(EYES)
+    });
   }
   return wizards;
 }
-createWizardsProperties();
 
 function renderWizard(wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -41,13 +37,18 @@ function renderWizard(wizard) {
   return wizardElement;
 }
 
-function createFragment() {
+function drewWizards(wizards) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < wizards.length; i++) {
     fragment.appendChild(renderWizard(wizards[i]));
   }
   similarListElement.appendChild(fragment);
 }
-createFragment();
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+function init() {
+  var wizards = createWizardsProperties();
+  drewWizards(wizards);
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  userDialog.classList.remove('hidden');
+}
+init();
